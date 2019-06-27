theKramer77/android_device_lge_h811

H811: Simplify and AOSPExtend the G4

aosp_h811.mk
@@ -18,12 +18,28 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from h815 device
+# Inherit ROM vendor common stuff
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+# Inherit ROM vendor telephony stuff
+$(call inherit-product, vendor/aosp/config/telephony.mk)
+
+# Overlays (inherit after vendor to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Inherit from h811 device
 $(call inherit-product, device/lge/h811/device.mk)
 
 # Set those variables here to overwrite the inherited values.
+RODUCT_NAME := aosp_h811
 PRODUCT_DEVICE := h811
-PRODUCT_NAME := full_h811
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H811
 PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g4" \
+    PRODUCT_NAME="g4__tmo_us" \
+    PRIVATE_BUILD_DESC="p1__tmo_us-user 8.0 NRD90U 1717709471d0f release-keys"
+
+BUILD_FINGERPRINT := "lge/p1__tmo_us/p1:8.1.0/OPM4.171019.016.A1/4720811:user/release-keys"

theKramer77/device_lge_h811

aosp_h811.mk
@@ -18,12 +18,28 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from h815 device
+# Inherit ROM vendor common stuff
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+# Inherit ROM vendor telephony stuff
+$(call inherit-product, vendor/aosp/config/telephony.mk)
+
+# Overlays (inherit after vendor to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Inherit from h811 device
 $(call inherit-product, device/lge/h811/device.mk)
 
 # Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := aosp_h811
 PRODUCT_DEVICE := h811
-PRODUCT_NAME := full_h811
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H811
 PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g4" \
+    PRODUCT_NAME="g4__tmo_us" \
+    PRIVATE_BUILD_DESC="p1__tmo_us-user 8.0 NRD90U 1717709471d0f release-keys"
+
+BUILD_FINGERPRINT := "lge/p1__tmo_us/p1:8.1.0/OPM4.171019.016.A1/4720811:user/release-keys"